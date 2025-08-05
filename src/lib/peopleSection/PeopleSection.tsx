2d6abda512ada272192bc36227df1016e747d0db
Timo Waszek <timowaszek@web.de>
2025-08-05 20:00:05 +0200

    add list and justify views

--- src/lib/peopleSection/PeopleSection.tsx
+++ src/lib/peopleSection/PeopleSection.tsx
@@ -3,22 +3,29 @@ import { StyledHeadline } from "../components/StyledHeadline";
 import { tRotate } from "../partnerSection/partnerSection.types";
 import { iPeople } from "./peopleSection.types";
 import { PeopleSectionItem } from "./PeopleSectionItem";
-
 interface PeopleSectionProps {
   headline: string;
   rotate?: tRotate;
   people: iPeople[];
+  isList?: boolean;
 }
 
 export const PeopleSection = ({
   headline,
   rotate,
   people,
+  isList = false, // 👈
 }: PeopleSectionProps) => {
   return (
     <div className="flex flex-col gap-y-10 justify-center items-center w-full">
       <StyledHeadline rotate={rotate} text={headline} />
-      <div className={`flex flex-wrap justify-around gap-6 w-full flex-row `}>
+      <div
+        className={`flex ${
+          isList
+            ? "flex-col items-center justify-evenly"
+            : "flex-wrap justify-around"
+        } gap-x-6 gap-y-6 w-full`}
+      >
         {people.map((person) => (
           <PeopleSectionItem key={person.uri} people={person} />
         ))}
